c4642e663574cb501a66f7e94003de3525551fda
Roy Chanunkha <roychanunkha@gmail.com>
2022-04-17 15:31:28 +0200

    making the redux store available to the whole app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '../styles/global.css'
+import store from '../state/store';
+import { Provider } from 'react-redux';
+
 function App({Component, pageProps}) {
-    return <Component {...pageProps} />
+    return (
+        <Provider>
+            <Component {...pageProps} />
+        </Provider>
+    )
 }
 
 export default App;
