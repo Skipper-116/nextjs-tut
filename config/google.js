08b8150050c45e4c5342aa278a61353fbefa4e5b
Roy Chanunkha <roychanunkha@gmail.com>
2022-04-18 13:49:02 +0200

    added react gapi auth2

---
+++ config/google.js
@@ -0,0 +1,5 @@
+export const ClientConfig = {
+    client_id: "772770906454-vfnsk9t53lqi0kasgsrummv7niiu4s2r.apps.googleusercontent.com",
+    cookie_policy: "single_host_origin",
+    scope: "https://www.googleapis.com/auth/calendar.events"
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import '../styles/global.css'
 import store from '../state/store';
 import { Provider } from 'react-redux';
+import { ClientConfig } from '../config/google';
+import { GApiProvider } from 'react-gapi-auth2';
 
 function App({Component, pageProps}) {
     return (
-        <Provider store={store}>
-            <Component {...pageProps} />
-        </Provider>
+        <GApiProvider clientConfig={ClientConfig}>
+            <Provider store={store}>
+                <Component {...pageProps} />
+            </Provider>
+        </GApiProvider>
     )
 }
 
