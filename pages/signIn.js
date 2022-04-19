4f13534c54e1042efa93a0f196c29fc693159e58
Roy Chanunkha <roychanunkha@gmail.com>
2022-04-19 20:44:20 +0200

    i believe i have done some good work on the signin page

--- pages/signIn.js
+++ pages/signIn.js
@@ -1,7 +1,7 @@
 import { useGoogleAuth } from "react-gapi-auth2";
 import Layout from '../components/layout'
 import { useRouter } from "next/router";
-import { setGoogleAuth, setCurrentUser } from "../state/google/googleSlice";
+import { setAccessToken } from "../state/google/googleSlice";
 import { useDispatch } from "react-redux";
 
 function SignIn(){
@@ -10,17 +10,20 @@ function SignIn(){
     const dispatch = useDispatch();
     //const currentUser = useGoogleUser();
     
-    if(googleAuth.googleAuth && googleAuth.googleAuth.isSignedIn.get()){
-        //const res = fetch()
-        dispatch(setGoogleAuth(googleAuth.googleAuth));
-        dispatch(setCurrentUser(googleAuth.googleAuth.currentUser.get()))
-        router.push('/event');
-    }
-
     return (
         <Layout>
             <p>Click here to sign in:</p>
-            <button onClick={() => googleAuth.googleAuth?.signIn()}>Sign In</button>
+            <button onClick={async () => {
+                await googleAuth.googleAuth.signIn();
+                if(googleAuth.googleAuth.isSignedIn){
+                    //const res = fetch()
+                    dispatch(setAccessToken(googleAuth.googleAuth.currentUser.get().getAuthResponse().access_token));
+                    router.push('/event');
+                }else{
+                    console.log("Failed to login bwana");
+                }
+            }
+            }>Sign In</button>
         </Layout>
     )
 }
