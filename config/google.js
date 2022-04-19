80c936a3f4914f5acacab19741dc199f9f021d1e
Roy Chanunkha <roychanunkha@gmail.com>
2022-04-19 20:43:06 +0200

    updated the google config file no more client id exposure

--- config/google.js
+++ config/google.js
@@ -1,5 +1,5 @@
 export const ClientConfig = {
-    client_id: process.env.CLIENT_ID,
-    cookie_policy: process.env.COOKIE_POLICY,
-    scope: process.env.SCOPE
+    client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+    cookie_policy: process.env.NEXT_PUBLIC_COOKIE_POLICY,
+    scope: process.env.NEXT_PUBLIC_SCOPE
 }
